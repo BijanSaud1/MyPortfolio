2b27cbfb37d4acb3fecc85f222f4aa7168ceffa6
BijanSaud1 <bxs8512@mavs.uta.edu>
2023-05-29 17:47:08 -0500

    4

---
+++ src/data/user.js
@@ -0,0 +1,84 @@
+const INFO = {
+	main: {
+		title: "React portfolio_BijanSaud ",
+		name: "Bijan Saud",
+		email: "saud123bijan@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/BijanSaud1",
+		linkedin: "https://www.linkedin.com/in/bijan-saud-013838195/",
+		instagram: "https://instagram.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Full-stack web and mobile Application developer: Aspiring Entrepreneur.",
+		description: "I am a Full-Stack developer with Experties in Java, C#, and React. I have experties in building sclable, secure, and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and foccus on learning new skills and technologies. I am passionate about creating high-quality code that follows best industry practices and standards. I am always seeking new challanges and opportunites to grow as a developer. ",
+			
+	},
+
+	about: {
+		title: "I'm Bijan Saud. I live in United States, where In my small appartment, I aspire to become an Developer and eventully an Entrepreneur.",
+		description:
+			"I've worked on a variety of projects over the years and I;m proud of the process that I have achieved. Many of the projects are in my GitHub and are available for other to explore. If you have any suggestions and feedback on my projects the please feel free to connect with me. I' am always open new ideas and thoughts to improve myself.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Employee Management System",
+			description:
+				"Employee Management System is a basic Management System that I developed using Java Spring Boot for the backend, SQL for database, and React for FrontEnd. This project performs all the CRUD operations. I also have used Spring Security with JSON webtoken for Authentication and Authorization.", 
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/BijanSaud1/EmployeeManagementSystem",
+		},
+
+		{
+			title: "Corp Catch",
+			description:
+				"Corp Catch is a Simple Mobile game Application developed for US-ARMY. In this Applicaiton We used Android Studio for developement using Dart and Flutter, and Firebase for database. With login and register setup Corp Catch allows user to chose a tail from the list of trails and take on a journey with interative trail question to earn Corp Coin.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/java/java.png",
+			linkText: "View Project",
+			link: "https://github.com/BijanSaud1/CoprCatch",
+		},
+
+		{
+			title: "Escape Room",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/csharp/csharp.png",
+			linkText: "View Project",
+			link: "https://github.com/BijanSaud1/EscapeRoom",
+		},
+
+		{
+			title: "Car Rentals",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/php/php.png",
+			linkText: "View Project",
+			link: "https://github.com/BijanSaud1/EscapeRoom",
+		},
+		{
+			title: "BlackJack",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com/BijanSaud1/EscapeRoom",
+		},
+
+	
+	],
+};
+
+export default INFO;
---
+++ src/pages/projects.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
+
+import NavBar from "../components/common/navBar";
+import Footer from "../components/common/footer";
+import Logo from "../components/common/logo";
+import AllProjects from "../components/projects/allProjects";
+
+import INFO from "../data/user";
+import SEO from "../data/seo";
+
+import "./styles/projects.css";
+
+const Projects = () => {
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+
+	const currentSEO = SEO.find((item) => item.page === "projects");
+
+	return (
+		<React.Fragment>
+			<Helmet>
+				<title>{`Projects | ${INFO.main.title}`}</title>
+				<meta name="description" content={currentSEO.description} />
+				<meta
+					name="keywords"
+					content={currentSEO.keywords.join(", ")}
+				/>
+			</Helmet>
+
+			<div className="page-content">
+				<NavBar active="projects" />
+				<div className="content-wrapper">
+					<div className="projects-logo-container">
+						<div className="projects-logo">
+							<Logo width={46} />
+						</div>
+					</div>
+					<div className="projects-container">
+						<div className="title projects-title">
+							Things that I have done.
+						</div>
+
+						<div className="subtitle projects-subtitle">
+						I've worked on a variety of projects over the years and 
+						I'm proud of the process that I have achieved. Many of the projects are in my GitHub and are available for other to explore. 
+						If you have any suggestions and feedback on my 
+						projects the please feel free to connect with me. I' am always open new ideas and thoughts to improve myself.
+						</div>
+
+						<div className="projects-list">
+							<AllProjects />
+						</div>
+					</div>
+					<div className="page-footer">
+						<Footer />
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default Projects;
